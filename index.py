231631a02fe2540660af6883ca9d4d2d37e22266
Lohith <69358508+lohithgsk@users.noreply.github.com>
2024-08-18 22:24:16 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,101 @@
+'''
+                                                                                                                                                                                                                                                                                                                                     
+ _|_|_|      _|_|_|    _|_|_|        _|_|_|  _|      _|    _|_|    _|_|_|    _|_|_|_|_|      _|      _|    _|_|    _|_|_|  _|      _|  _|_|_|_|_|  _|_|_|_|  _|      _|    _|_|    _|      _|    _|_|_|  _|_|_|_|        _|_|    _|_|_|    _|_|_|    _|        _|_|_|    _|_|_|    _|_|    _|_|_|_|_|  _|_|_|    _|_|    _|      _|  
+ _|    _|  _|        _|            _|        _|_|  _|_|  _|    _|  _|    _|      _|          _|_|  _|_|  _|    _|    _|    _|_|    _|      _|      _|        _|_|    _|  _|    _|  _|_|    _|  _|        _|            _|    _|  _|    _|  _|    _|  _|          _|    _|        _|    _|      _|        _|    _|    _|  _|_|    _|  
+ _|_|_|      _|_|    _|  _|_|        _|_|    _|  _|  _|  _|_|_|_|  _|_|_|        _|          _|  _|  _|  _|_|_|_|    _|    _|  _|  _|      _|      _|_|_|    _|  _|  _|  _|_|_|_|  _|  _|  _|  _|        _|_|_|        _|_|_|_|  _|_|_|    _|_|_|    _|          _|    _|        _|_|_|_|      _|        _|    _|    _|  _|  _|  _|  
+ _|              _|  _|    _|            _|  _|      _|  _|    _|  _|    _|      _|          _|      _|  _|    _|    _|    _|    _|_|      _|      _|        _|    _|_|  _|    _|  _|    _|_|  _|        _|            _|    _|  _|        _|        _|          _|    _|        _|    _|      _|        _|    _|    _|  _|    _|_|  
+ _|        _|_|_|      _|_|_|      _|_|_|    _|      _|  _|    _|  _|    _|      _|          _|      _|  _|    _|  _|_|_|  _|      _|      _|      _|_|_|_|  _|      _|  _|    _|  _|      _|    _|_|_|  _|_|_|_|      _|    _|  _|        _|        _|_|_|_|  _|_|_|    _|_|_|  _|    _|      _|      _|_|_|    _|_|    _|      _|  
+                                                                                                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                                                                                                     
+WebApplication to report College Facility Maintenance
+BY AADITYA RENGARAJAN
+CREATION TIMESTAMP : 22:53 12/22/21 22 12 2021
+[TO-DO]
+- Change Flask Port
+- Remove Debuggers on Production Deployment
+'''
+#==============IMPORTING MODULES======================================================
+#/- see 'requirements.txt' to install extra modules via pip
+from flask import redirect, render_template, Flask, request, make_response, send_file
+from modules import qrgen
+
+#==============DEFINING BASIC FUNCTIONS======================================================
+app = Flask(__name__)
+#==============ROUTES======================================================
+#/- routes are defined by @app.route decorator
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_file("thumb.png")
+
+@app.route('/')
+def index():
+    if request.args.get("options")==None:
+        return render_template("index.html")
+    else:
+        qrgen.makeresult(request.args.get("options"),
+            {"block": request.args.get("block"),
+                "floor": request.args.get("floor"),
+                "room" : request.args.get("room"),
+                "department" : request.args.get("department"),
+                "dispenser_no" : request.args.get("dispenser")})
+        with open('qrcode_result.pdf', 'rb') as f:
+            file_content = f.read()
+        response = make_response(file_content, 200)
+        response.headers['Content-Disposition'] = f'attachment; filename="QR_{request.args.get("options").upper()}_{request.args.get("block")}{request.args.get("floor")}.pdf"'
+        response.headers['Content-type'] = 'application/pdf'
+        return response   
+        return send_file("qrcode_result.pdf", mimetype='application/pdf', as_attachment=False)
+
+
+#==============ERROR HANDLING======================================================
+#/- custom error handling using custom template to get fancy ;)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return str((render_template('error.html',
+                code="404",
+                type="Not Found",
+                content="Sorry, this page was not found!"))), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return str((render_template('error.html',
+                code="500",
+                type="Internal Server Error",
+                content=f"Oh No! Something Went Wrong!<br/>{e}"))), 500
+
+@app.errorhandler(410)
+def gone(e):
+    return str((render_template('error.html',
+                code="410",type="Gone",
+                content="Sorry, this page is has mysteriously vanished!"))), 410
+
+@app.errorhandler(403)
+def forbidden(e):
+    return str((render_template('error.html',
+                code="403",
+                type="Forbidden",
+                content="Sorry, you are not allowed to access this page!"))), 403
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return str((render_template('error.html',
+                code="401",
+                type="Unauthorized",
+                content="Sorry, you are not authorized to access this page!"))), 401
+#==============PROGRAM RUN======================================================
+if __name__=="__main__":
+    #/- note : remove debuggers and change port respectively
+    #/- on production deployment.
+    app.run(
+        debug=True,
+        use_reloader=True,
+        use_debugger=True,
+        port=7500,
+	host='0.0.0.0',
+        use_evalex=True,
+        threaded=True,
+        passthrough_errors=False
+        )
+#==============END OF WEBAPP======================================================
---
+++ modules/qrgen.py
@@ -0,0 +1,105 @@
+import qrcode 
+from PIL import Image
+if not hasattr(Image, 'Resampling'):  # Pillow<9.0
+    Image.Resampling = Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+def getQRcode(strs, name):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border =5
+    )
+    qr.add_data(strs)
+    qr.make(fit = True)
+    img = qr.make_image(fill_color = "black", back_color = "white")
+    img = img.convert("CMYK")
+    icon = Image.open("static/psggroups.png")
+    img_w, img_h = img.size
+    factor = 6
+    size_w = int(img_w / factor)
+    size_h = int(img_h / factor)
+    icon_w, icon_h = icon.size
+    if icon_w > size_w:
+        icon_w = size_w
+    if icon_h > size_h:
+        icon_h = size_h
+    icon = icon.resize((icon_w, icon_h), Image.Resampling.LANCZOS)
+    w = int((img_w - icon_w) / 2)
+    h = int((img_h - icon_h) / 2)
+    # img.paste(icon, (w, h), None)
+    img = img.convert('RGB')
+    img.save(name)
+    return img
+
+def info(name, body, title=""):
+    getQRcode(body, name)
+    oriImg = Image.open("static/background.png")
+    oriImg2 = Image.open(name)
+    oriImg2 = oriImg2.resize((1500,1500))
+    oriImg.paste(oriImg2, (500,1250))
+    draw = ImageDraw.Draw(oriImg)
+    oriImg = oriImg.convert('RGB')
+    draw = ImageDraw.Draw(oriImg)
+    font = ImageFont.truetype("static/j9_bold.ttf", 225)
+    W, H = oriImg.size
+    msg = title
+    w  = draw.textlength(msg)
+    wid = (W-w)/3
+    if len(msg)>5: wid = (W-w)/4
+    if len(msg)>8: wid = (W-w)/6
+    if len(msg)>10: wid = (W-w)/10
+    draw.text((wid, 750),msg,(0,0,0),font=font)
+    oriImg.save(name.replace(".png",".pdf"))
+
+def makeresult(prefill_type, data, BASE_URL="gms.psgtech.ac.in"):
+    if prefill_type=="classroom":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"],
+                    "room"  : data["room"]
+                  }
+        title = data["room"]
+    elif prefill_type=="department":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"],
+                    "department"  : data["department"]
+                  }
+        title = data["department"]
+    elif prefill_type=="restroom":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"]
+                  }
+        title = f"{data['block']}{data['floor']} W/C"
+    elif prefill_type=="water":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"],
+                    "disp"  : data["dispenser_no"]
+                  }
+        title = f"Water Doctor {data['dispenser_no']}"
+    elif prefill_type=="lift":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"]
+                  }
+        title = f"{block} Block Lift"
+    elif prefill_type=="misc":
+        prefill = {
+                    "block" : data["block"],
+                    "floor" : data["floor"]
+                  }
+        title = f"{data['block']} Block | Floor {data['floor']}"
+    route = f"/../issue/report?choice={prefill_type}&block={prefill['block']}&floor={prefill['floor']}"
+    if prefill.get("room"): route+=f"&room={prefill['room']}"
+    if prefill.get("department"): route+=f"&department={prefill['department']}"
+    if prefill.get("disp"): route+=f"&disp={prefill['disp']}"
+    url = f"{route}"
+    info("qrcode_result.png", url, title)
+
+if __name__ == '__main__':
+    pass
